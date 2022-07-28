70d0bd4552864d7f756d35a1b2e10b18dade6242
xSami2 <saami2206@gmail.com>
2022-07-28 10:34:04 +0300

    34

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use App\Models\Category;
 */
 Route::get('/', function () {
      return view('posts',[
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+         'categories' => Category::all()
     ]);
 });
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -31,12 +32,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 });
 Route::get("categories/{category:slug}", function(Category $category) {
     return view("posts", [
-        "posts"=>$category->posts
+        "posts"=>$category->posts,
+        "currentCategory" => $category,
+        'categories' => Category::all()
     ]);
 });
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts()->with('author')->get()
+        'posts' => $author->posts()->with('author')->get(),
+        'categories' => Category::all()
     ]);
 });
 
