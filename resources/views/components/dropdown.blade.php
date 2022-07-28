2187957efb1fd109a8e58fc37fb4cd6d3d2a8896
xSami2 <saami2206@gmail.com>
2022-07-28 12:42:16 +0300

    35

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,14 @@
+@props(['$trigger'])
+<div>
+
+    <div x-data="{show:false } "@click.away="show=false">
+        {{--Trigger--}}
+       <div @click="show=!show" >
+           {{$trigger}}
+       </div>
+        {{--Links--}}
+        <div x-show = "show" class="py-2 absolute bg-gray-100 w-32 mt-2 rounded-xl z-50">
+
+            {{$slot}}
+        </div>
+    </div>
