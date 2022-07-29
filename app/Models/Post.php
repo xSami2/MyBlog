9c2145bd6c6a13876b73fdbb72b161ae8175c173
xSami2 <saami2206@gmail.com>
2022-07-29 19:05:17 +0300

    43

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,9 +18,10 @@ class Post extends Model
     public function scopeFilter($query , array $filters)
     {
         $query->when($filters['search'] ?? false , fn($query ,$search) =>
-            $query
-                ->where('title', 'like', '%' . request("search") . '%')
-                ->orWhere('body', 'like', '%' . request("search") . '%'));
+            $query->where(fn ($query) =>
+            $query->where('title', 'like', '%' . request("search") . '%')
+                ->orWhere('body', 'like', '%' . request("search") . '%')));
+
         $query->when($filters['category'] ?? false , fn ($query , $category) =>
             $query->whereHas("category", fn($query) =>
             $query->where('slug', $category)));
