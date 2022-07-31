6391c29ba50cc70bb403e4f00ccce29f9434693c
xSami2 <saami2206@gmail.com>
2022-07-31 15:35:37 +0300

    47

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -17,13 +18,15 @@ class RegisterController extends Controller
         // create the user
 
        $attributes = request()->validate([
-            'name' => 'required|max:255',
-            'username'=>'required|max:255|min:3',
-            'email'=>'required|email|max:255',
+            'name' => ['required' , 'max:255' , 'min:3'],
+            'username'=>['required' , 'max:255' , Rule::unique('users','username')],
+            'email'=>['required' , 'email' ,Rule::unique('users','email')],
             'password'=>['required' , 'max:255' , 'min:3']  // Another Way  | Password Bcrypt in User.php
         ]);
 
            User::create($attributes);
+
+
            return redirect('/');
 
     }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -11,26 +11,38 @@
                              @csrf
                   <div class="mb-6">
                       <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
-                      <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
+                      <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" value="{{old("name")}}" required>
+                      @error('name')
+                      <p class="text-red-500 text-xs mt-1 font-bold">{{$message}}</p>
+                      @enderror
                   </div>
 
                    <div class="mb-6">
                   <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">Username</label>
-                  <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
+                  <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" value="{{old("username")}}" required>
+                       @error('username')
+                       <p class="text-red-500 text-xs mt-1 font-bold">{{$message}}</p>
+                       @enderror
                   </div>
 
                   <div class="mb-6">
                       <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
-                      <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
+                      <input class="border border-gray-400 p-2 w-full" type="email" name="email"  id="email"value="{{old("email")}}" required>
+                      @error('email')
+                      <p class="text-red-500 text-xs mt-1 font-bold">{{$message}}</p>
+                      @enderror
                   </div>
 
                   <div class="mb-6">
                       <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
-                      <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
+                      <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"  required>
+                      @error('password')
+                      <p class="text-red-500 text-xs mt-1 font-bold">{{$message}}</p>
+                      @enderror
                   </div>
 
                   <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
+                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 ">Submit</button>
                   </div>
 
 
