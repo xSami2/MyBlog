0d1132c192d8da8ad673944f796bd2a44bce6269
xSami2 <saami2206@gmail.com>
2022-07-31 13:03:02 +0300

    45

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use \App\Http\Controllers\PostController;
 use Couchbase\User;
@@ -8,9 +10,15 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Models\Category;
 
 
+
+
 Route::get('/', [PostController::class,'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class , 'show']);
+
+Route::get('register',[RegisterController::class , 'create'] );
+Route::post('register',[RegisterController::class , 'store'] );
+
 
 
 
